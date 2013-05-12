8942f836162bf1ce4adabd13612a7c5e93e8cb4c
Christopher Lopes <christopher.lopes@gmail.com>
2013-05-12 12:58:44 -0700

    Apply smartborders to all Xmonad layouts
    Change the way smartborders was being applied so it applies to all
    layouts. Also, only keep the xmonad config file in the xmonad
    directory, remove the others.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -186,7 +186,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = avoidStruts(smartBorders Tall tiled ||| smartBorders Mirror tiled ||| noBorders Full)
+myLayout = avoidStruts $ smartBorders $ tiled ||| Mirror tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
