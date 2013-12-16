90f21612b559d0cc569cf58c9b1c638e6e90b04e
Christopher Lopes <christopher.lopes@gmail.com>
2013-12-15 17:22:30 -0800

    Move from xmonad to bspwm

---
+++ .lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user {
+        :plugins [[lein-exec "0.3.1"]
+                  [lein-try "0.4.1"]
+                  [lein-kibit "0.0.8"]
+                  [lein-ancient "0.5.4"]
+                  [lein-midje "3.1.1"]]
+        :dependencies [[slamhound "1.5.0"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}}
