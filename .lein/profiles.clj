4a9d640947ca597cd5180dba1d48128d10874c36
Christopher Lopes <christopher.lopes@gmail.com>
2015-01-05 19:36:32 -0800

    Update lein plugins and dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,10 +3,10 @@
                   [lein-try "0.4.3"] ; try lib w/o including it as a depencies
                   [lein-kibit "0.0.8"] ; search for more idiomatic function
                   [lein-ancient "0.5.5"] ; upgrade dependencies
-                  [cider/cider-nrepl "0.8.0-SNAPSHOT"] ; only used to run nrepl using cider in emacs
+                  [cider/cider-nrepl "0.8.2-SNAPSHOT"] ; only used to run nrepl using cider in emacs
                   [lein-midje "3.1.3"] ; testing
                   ]
-        :dependencies [[spyscope "0.1.4"] ; insert tracing statements
+        :dependencies [[spyscope "0.1.5"] ; insert tracing statements
                        [leiningen "2.4.3"] ; required by vinyasa
                        ;[org.clojure/tools.namespace "0.2.5"] ; reload namespaces
                        [slamhound "1.5.5"] ; tidy up namespaces
@@ -23,11 +23,11 @@
                      (require '[vinyasa.inject :as inject])
                      (inject/in
                        clojure.core >
-                      ;[clj-ns-browser.sdoc sdoc]
-                        ;[midje.repl autotest]
-                        [alex-and-georges.debug-repl debug-repl]; (>debug-repl) anywhere; () to continue
-                        [clojure.tools.namespace.repl refresh]; (>refresh) to reload the repl
-                        [clojure.repl doc source]
-                        [clojure.pprint pprint pp]); (apply + (doto (range [11 21 2]) >pprint)
-        ]
+                       ;[clj-ns-browser.sdoc sdoc]
+                       ;[midje.repl autotest]
+                       [alex-and-georges.debug-repl debug-repl]; (>debug-repl) anywhere; () to continue
+                       [clojure.tools.namespace.repl refresh]; (>refresh) to reload the repl
+                       [clojure.repl doc source]
+                       [clojure.pprint pprint pp]); (apply + (doto (range [11 21 2]) >pprint)
+                     ]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
